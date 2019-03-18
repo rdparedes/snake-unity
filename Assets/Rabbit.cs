7a32b43e539a7e1d665c315b69220bbeb56dd49f
Roberto Paredes <rdparedessalazar@gmail.com>
2019-03-18 06:56:53 -0500

    Spawn rabbit only in collision-free positions

--- Assets/Rabbit.cs
+++ Assets/Rabbit.cs
@@ -4,7 +4,17 @@ public class Rabbit : MonoBehaviour
 {
     private void Start()
     {
-        transform.position = new Vector2(
+        Vector2 newPosition;
+        do
+        {
+            newPosition = generateRandomPosition();
+        } while (Physics.CheckBox(newPosition, new Vector2(0.5f, 0.5f)) == true);
+        transform.position = newPosition;
+    }
+
+    private Vector2 generateRandomPosition()
+    {
+        return new Vector2(
             Random.Range((int)Config.GridSize.x, (int)Config.GridSize.width),
             Random.Range((int)Config.GridSize.y, (int)Config.GridSize.height));
     }
