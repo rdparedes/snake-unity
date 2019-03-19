8a249ca848a9d0880d8f10ce4a8c5112b1e6d977
Roberto Paredes <rdparedessalazar@gmail.com>
2019-03-19 09:50:50 -0500

    Adjust screen size settings

--- Assets/Config.cs
+++ Assets/Config.cs
@@ -4,6 +4,7 @@ public class Config : MonoBehaviour
 {
     // All objects have 0,5 offset with respect to the Grid
     const float GRID_OFFSET = .5f;
+    const float SCENE_SIZE = 10;
     public static Rect GridSize { get; private set; }
 
     public static void Init()
@@ -11,8 +12,7 @@ public class Config : MonoBehaviour
         GridSize = new Rect(
             GRID_OFFSET,
             GRID_OFFSET,
-            (Screen.width / 16) + GRID_OFFSET,
-            (Screen.height / 16) + GRID_OFFSET);
-
+            SCENE_SIZE + GRID_OFFSET,
+            SCENE_SIZE + GRID_OFFSET);
     }
 }
